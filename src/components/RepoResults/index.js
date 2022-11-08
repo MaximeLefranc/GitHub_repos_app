383c1c021d67b498ddac04e31554d040a98053a4
SoleneOclock <solene@oclock.io>
2022-11-08 09:47:16 +0100

    integration statique terminée

--- src/components/RepoResults/index.js
+++ src/components/RepoResults/index.js
@@ -1,8 +1,27 @@
+import { Card } from 'semantic-ui-react';
+
 function ReposResults() {
   return (
-    <div>
-      ReposResults
-    </div>
+    <Card.Group itemsPerRow={3}>
+      <Card
+        image="https://avatars3.githubusercontent.com/u/698437?v=4"
+        header="React"
+        meta="React"
+        description="Un super framework !"
+      />
+      <Card
+        image="https://avatars3.githubusercontent.com/u/698437?v=4"
+        header="React"
+        meta="React"
+        description="Un super framework !"
+      />
+      <Card
+        image="https://avatars3.githubusercontent.com/u/698437?v=4"
+        header="React"
+        meta="React"
+        description="Un super framework !"
+      />
+    </Card.Group>
   );
 }
 
