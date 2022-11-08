eda6374d8369287fa8b04925ffed0e4a9e6646a7
SoleneOclock <solene@oclock.io>
2022-11-08 10:07:32 +0100

    Input controlé (par search dans le state de App) dans SearchBar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import logo from '../../assets/images/logo-github.png';
 import ReposResults from '../RepoResults';
 import Message from '../Message';
@@ -5,12 +7,16 @@ import SearchBar from '../SearchBar';
 import './styles.scss';
 
 function App() {
+  /* on créé un emplacement dans le state de App pour stocker la valeur de l'input,
+  on donne cette valeur et la fonction qui permet de la modifier à SearchBar : via les props */
+  const [search, setSearch] = useState('chocolat');
+
   const message = "Coucou";
 
   return (
     <div className="app">
       <img src={logo} alt="logo Github" />
-      <SearchBar />
+      <SearchBar search={search} setSearch={setSearch} />
       <Message message={message} />
       <ReposResults />
     </div>
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,6 +1,7 @@
+import PropTypes from 'prop-types';
 import { Input, Form, Segment } from 'semantic-ui-react';
 
-function SearchBar() {
+function SearchBar({ search, setSearch }) {
   return (
     <Form>
       <Segment>
@@ -9,10 +10,21 @@ function SearchBar() {
           icon="search"
           placeholder="Search..."
           iconPosition="left"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
         />
       </Segment>
     </Form>
   );
 }
+
+SearchBar.propTypes = {
+  // la valeur de l'input depuis le state de App
+  search: PropTypes.string.isRequired,
+  // la fonction pour modifier cette valeur dans le state de App
+  setSearch: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
