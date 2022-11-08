497fe59103466fcc16853b724cbe53e06ab03bb6
SoleneOclock <solene@oclock.io>
2022-11-08 12:32:38 +0100

    Ajout d'un segment sur la page Faq

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -1,26 +1,29 @@
-import { List } from 'semantic-ui-react';
+import { List, Segment } from 'semantic-ui-react';
 
 function Faq() {
   return (
-    <List>
-      <List.Item>
-        <List.Content>
-          <List.Header>A quoi ça sert ?</List.Header>
-          <List.Description>
-            Cette application permet de trouver une liste de dépôts GitHub pour un critère donné.
-          </List.Description>
-        </List.Content>
-      </List.Item>
+    <Segment>
+      <h1>FAQ</h1>
+      <List>
+        <List.Item>
+          <List.Content>
+            <List.Header>A quoi ça sert ?</List.Header>
+            <List.Description>
+              Cette application permet de trouver une liste de dépôts GitHub pour un critère donné.
+            </List.Description>
+          </List.Content>
+        </List.Item>
 
-      <List.Item>
-        <List.Content>
-          <List.Header>Comment faire une recherche ?</List.Header>
-          <List.Description>
-            Sur la page recherche, complétez le champ de recherche et valider la recherche.
-          </List.Description>
-        </List.Content>
-      </List.Item>
-    </List>
+        <List.Item>
+          <List.Content>
+            <List.Header>Comment faire une recherche ?</List.Header>
+            <List.Description>
+              Sur la page recherche, complétez le champ de recherche et valider la recherche.
+            </List.Description>
+          </List.Content>
+        </List.Item>
+      </List>
+    </Segment>
   );
 }
 
