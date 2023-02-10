f5b9ad9597a4b7c4c949b712d72bb9504b3241ff
Maxime <maxilefranc@gmail.com>
2023-02-10 11:30:29 +0100

    add Links to cards for go to repository git hub clicked

--- src/components/RepoResults/index.js
+++ src/components/RepoResults/index.js
@@ -4,21 +4,19 @@ import PropTypes from 'prop-types';
 function ReposResults({ repos }) {
   // on a repos qui est un tableau d'objets
   // on veut un tableau de Card pour notre JSX
-
   return (
     <>
       <Card.Group itemsPerRow={3}>
-        {
-          repos.map((item) => (
-            <Card
-              image={item.owner.avatar_url}
-              header={item.name}
-              meta={item.full_name}
-              description={item.description}
-              key={item.id}
-            />
-          ))
-        }
+        {repos.map((item) => (
+          <Card
+            image={item.owner.avatar_url}
+            header={item.name}
+            meta={item.full_name}
+            description={item.description}
+            key={item.id}
+            href={item.html_url}
+          />
+        ))}
       </Card.Group>
       {/* affichage conditionnel : on affiche que si il reste des resultats non chargés */}
       {/* totalCount > repos.length && <MoreButton fetchMoreRepo={fetchMoreRepo} /> */}
@@ -28,13 +26,15 @@ function ReposResults({ repos }) {
 
 ReposResults.propTypes = {
   // la liste des repos à afficher sous forme de Card
-  repos: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    owner: PropTypes.shape({
-      avatar_url: PropTypes.string.isRequired,
-    }).isRequired,
-  })).isRequired,
+  repos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      owner: PropTypes.shape({
+        avatar_url: PropTypes.string.isRequired,
+      }).isRequired,
+    }),
+  ).isRequired,
 };
 
 export default ReposResults;
